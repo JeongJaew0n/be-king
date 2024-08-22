1baa7037fbf214f272b00fb3e0bb5a962377cc71
HeLLo2 <popt0@naver.com>
2024-08-22 23:14:03 +0900

    chore: 프로젝트 init

---
+++ src/main/java/com/ohell/beking/BeKingApplication.java
@@ -0,0 +1,13 @@
+package com.ohell.beking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohell/beking/BeKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohell.beking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
