c314605cbbce0bec44b854634b2620d9a99b8406
HeLLo2 <popt0@naver.com>
2024-08-25 22:42:25 +0900

    chore: db 연결 해제 상태 만들기

--- src/main/java/com/ohell/beking/BeKingApplication.java
+++ src/main/java/com/ohell/beking/BeKingApplication.java
@@ -2,8 +2,9 @@ package com.ohell.beking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BeKingApplication {
 
     public static void main(String[] args) {
